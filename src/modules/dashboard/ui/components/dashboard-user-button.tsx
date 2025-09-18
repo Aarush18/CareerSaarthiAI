40c0149cbc204322697f31007c3c3762fa66ec8b
Aarush Gupta <aarushgupta2018@gmail.com>
2025-09-18 22:08:12 +0530

    Miscellanious change comment vale changes

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,8 +1,9 @@
+"use client"
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { useIsMobile } from "@/hooks/use-mobile";
-// import {authClient} from "@/lib/auth-client"
+import {authClient} from "@/lib/auth-client"
 
 import {
   Drawer,
@@ -24,31 +25,33 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ChevronDown, ChevronDownIcon, CreditCardIcon, LogOut, LogOutIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export const DashboardUserButton = () => {
-  // const {data , isPending} = authClient.useSession();
+  const {data , isPending} = authClient.useSession();
   const isMobile = useIsMobile();
+  const router = useRouter()
 
-//   const onLogout = () => {
-//       authClient.signOut({
-//         fetchOptions:{
-//             onSuccess : () => {
-//                 router.push("/sign-in");
-//             }
-//         }
-//      });
-//   }
+  const onLogout = () => {
+      authClient.signOut({
+        fetchOptions:{
+            onSuccess : () => {
+                router.push("/sign-in");
+            }
+        }
+     });
+  }
 
 
-  // if(isPending || !data?.user){
-  //     return null;
-  // }
+  if(isPending || !data?.user){
+      return null;
+  }
 
   if(isMobile){
     return (
       <Drawer>
         <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
-             {/* { {data.user.image ? (
+             {data?.user.image ? (
               <Avatar>
                 <AvatarImage src={data.user.image}/>
               </Avatar>
@@ -58,18 +61,18 @@ export const DashboardUserButton = () => {
              variant = "initials"
              className = "size-9 mr-3"
              />
-             )} } */}
+             )}
         <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
-          {/* <p className="text-sm truncate w-full">{data.user.name}</p>
-          <p className="text-xs truncate w-full">{data.user.email}</p> */}
+          <p className="text-sm truncate w-full">{data.user.name}</p>
+          <p className="text-xs truncate w-full">{data.user.email}</p>
         </div>
         <ChevronDownIcon className="size-5 shrink-0" />
         </DrawerTrigger>
         <DrawerContent>
           <DrawerHeader>
-            {/* <DrawerTitle>{data.user.name}</DrawerTitle> */}
+            <DrawerTitle>{data.user.name}</DrawerTitle>
             <DrawerDescription>
-              {/* {data.user.email} */}
+              {data.user.email}
             </DrawerDescription>
           </DrawerHeader>
           <DrawerFooter>
@@ -82,7 +85,7 @@ export const DashboardUserButton = () => {
             </Button>
             <Button
             variant={"outline"}
-            // onClick={() => onLogout()}
+            onClick={() => onLogout()}
             >
               <LogOutIcon className="size-4 text-black"/>
               Logout
@@ -96,7 +99,7 @@ export const DashboardUserButton = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
-        {/* { {data.user.image ? (
+        {data.user.image ? (
               <Avatar>
                 <AvatarImage src={data.user.image}/>
               </Avatar>
@@ -106,20 +109,20 @@ export const DashboardUserButton = () => {
              variant = "initials"
              className = "size-9 mr-3"
              />
-             )} } */}
+             )}
         <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
-          {/* <p className="text-sm truncate w-full">{data.user.name}</p>
-          <p className="text-xs truncate w-full">{data.user.email}</p> */}
+          <p className="text-sm truncate w-full">{data.user.name}</p>
+          <p className="text-xs truncate w-full">{data.user.email}</p>
         </div>
         <ChevronDownIcon className="size-5 shrink-0" />
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" side="right" className="w-72">
         <DropdownMenuLabel>
           <div className="flex flex-col gap-1">
-            {/* <span className="font-medium truncate">{data.user.name}</span>
+            <span className="font-medium truncate">{data.user.name}</span>
             <span className="text-sm font-normal text-muted-foreground truncate">
               {data.user.email}
-            </span> */}
+            </span>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
@@ -127,10 +130,10 @@ export const DashboardUserButton = () => {
             Billing
             <CreditCardIcon className="size-4"/>
         </DropdownMenuItem>
-        {/* <DropdownMenuItem onClick={onLogout}>
+        <DropdownMenuItem onClick={onLogout}>
             Logout
             <LogOut className="size-4"/>
-        </DropdownMenuItem> */}
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
