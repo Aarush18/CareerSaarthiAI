ba58f40d07ba923ebfbbb648c0c29742bea4f818
devvarth Singh <devvarthsinghwork@gmail.com>
2025-09-20 20:10:56 +0530

    agent filters setup done

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -7,9 +7,11 @@ import { createTRPCContext } from '@trpc/tanstack-react-query';
 import { useState } from 'react';
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
+
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
+
 let browserQueryClient: QueryClient;
-function getQueryClient() {
+export function getQueryClient() {
   if (typeof window === 'undefined') {
     // Server: always make a new query client
     return makeQueryClient();
